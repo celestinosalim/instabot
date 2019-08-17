c3163f200ff28f963baac750d51a256bec4a77bf
celestinosalim <celestino.salim@gmail.com>
2019-08-17 13:21:06 -0400

    hide keys, password changed aswell

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
 const ig = require("./instagram");
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
 
 (async () => {
   await ig.initialize();
