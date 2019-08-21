0214d108a993fc6c6d0b42a4995e7fb86e5fa630
celestinosalim <celestino.salim@gmail.com>
2019-08-21 18:47:28 -0400

    testing get to ping

--- index.js
+++ index.js
@@ -17,32 +17,36 @@ app.listen(port, function() {
   console.log("App listening on port " + port);
 });
 
+app.get("/", function(req, res) {
+  res.send("hello world");
+
+  setInterval(() => {
+    (async () => {
+      await puppeteer
+        .launch({
+          args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-web-security"
+          ],
+          headless: true
+        })
+        .then(async browser => {
+          page = await browser.newPage();
+          await page.setUserAgent(process.env.USER_AGENT);
+          await page.emulate(device);
+          await login(username, password, page, BASE_URL, randomWait);
+          await postPhoto(page, randomWait);
+          await page.waitFor(10000);
+          await page.close();
+          await browser.close();
+        })
+        .catch(err => console.log(err));
+      // debugger;
+    })();
+  }, 60000 * 3);
+});
+
 function randomWait(min, max) {
   return Math.random() * (max - min) + min;
 }
-
-setInterval(() => {
-  (async () => {
-    await puppeteer
-      .launch({
-        args: [
-          "--no-sandbox",
-          "--disable-setuid-sandbox",
-          "--disable-web-security"
-        ],
-        headless: true
-      })
-      .then(async browser => {
-        page = await browser.newPage();
-        await page.setUserAgent(process.env.USER_AGENT);
-        await page.emulate(device);
-        await login(username, password, page, BASE_URL, randomWait);
-        await postPhoto(page, randomWait);
-        await page.waitFor(10000);
-        await page.close();
-        await browser.close();
-      })
-      .catch(err => console.log(err));
-    // debugger;
-  })();
-}, 60000 * 60 * 3);
