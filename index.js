ec8e241b1129dcf4072daf5ff2384a85612d68dd
celestinosalim <celestino.salim@gmail.com>
2019-08-21 16:39:26 -0400

    added time to randomwait

--- index.js
+++ index.js
@@ -21,28 +21,28 @@ function randomWait(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-// setInterval(() => {
-(async () => {
-  await puppeteer
-    .launch({
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-web-security"
-      ],
-      headless: false
-    })
-    .then(async browser => {
-      page = await browser.newPage();
-      await page.setUserAgent(process.env.USER_AGENT);
-      await page.emulate(device);
-      await login(username, password, page, BASE_URL, randomWait);
-      await postPhoto(page, randomWait);
-      await page.waitFor(10000);
-      await page.close();
-      await browser.close();
-    })
-    .catch(err => console.log(err));
-  // debugger;
-})();
-// }, 60000 * 3);
+setInterval(() => {
+  (async () => {
+    await puppeteer
+      .launch({
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-web-security"
+        ],
+        headless: false
+      })
+      .then(async browser => {
+        page = await browser.newPage();
+        await page.setUserAgent(process.env.USER_AGENT);
+        await page.emulate(device);
+        await login(username, password, page, BASE_URL, randomWait);
+        await postPhoto(page, randomWait);
+        await page.waitFor(10000);
+        await page.close();
+        await browser.close();
+      })
+      .catch(err => console.log(err));
+    // debugger;
+  })();
+}, 60000 * 3);
