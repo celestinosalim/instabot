55d3bc316a1c659e8137f4125269be227ec6d680
celestinosalim <celestino.salim@gmail.com>
2019-08-21 15:19:45 -0400

    deploying on heroku

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const device = require("puppeteer/DeviceDescriptors")["iPhone X"];
 const login = require("./login");
 const postPhoto = require("./postPhoto");
 
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
 const BASE_URL = process.env.BASE_URL;
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 let page;
+
+app.listen(port, function() {
+  console.log("App listening on port " + port);
+});
 
 function randomWait(min, max) {
   return Math.random() * (max - min) + min;
@@ -16,7 +24,14 @@ function randomWait(min, max) {
 // setInterval(() => {
 (async () => {
   await puppeteer
-    .launch({ args: ["--disable-web-security"], headless: false })
+    .launch({
+      args: [
+        "--disable-web-security",
+        "--no-sandbox",
+        "--disable-setuid-sandbox"
+      ],
+      headless: false
+    })
     .then(async browser => {
       page = await browser.newPage();
       await page.setUserAgent(process.env.USER_AGENT);
