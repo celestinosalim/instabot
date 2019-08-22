b2ee5682d2beee1d1714d728f4c4a1a49046f73f
celestinosalim <celestino.salim@gmail.com>
2019-08-21 23:33:19 -0400

    changing route

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.listen(port, function() {
   console.log("App listening on port " + port);
 });
 
-app.get("/", function(req, res) {
+app.get("/hello", function(req, res) {
   res.send("hello world");
 
   // setInterval(() => {
